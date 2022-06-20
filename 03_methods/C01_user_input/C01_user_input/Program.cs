6a7f6fb77b35424a2c99dd52090da68d27531a45
aljaz-kokol <52831109+aljaz-kokol@users.noreply.github.com>
2022-06-20 15:06:59 +0200

    03 methods

---
+++ 03_methods/C01_user_input/C01_user_input/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace C01_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Calculate());
+        }
+
+        public static int Calculate()
+        {
+            Console.Write("Please enter your first number: ");
+            string num1Input = Console.ReadLine();
+
+            Console.Write("Please enter your second number: ");
+            string num2Input = Console.ReadLine();
+            
+            int num1 = int.Parse(num1Input);
+            int num2 = int.Parse(num2Input);
+            return num1 + num2;
+            
+        }
+    }
+}
---
+++ 03_methods/C02_try_catch_finally/C02_try_catch_finally/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C02_try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            try
+            {
+                int userInputAsInt = int.Parse(Console.ReadLine());
+                Console.WriteLine(userInputAsInt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Format exception please enter the correct type next time");
+            }
+            finally
+            {
+                Console.WriteLine("This will get called no matter what");
+            }
+        }
+    }
+}
