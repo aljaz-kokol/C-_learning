033cbd7fa84c375fe915c1fbfc84d404dd08e920
aljaz-kokol <52831109+aljaz-kokol@users.noreply.github.com>
2022-07-04 13:59:51 +0200

    ok

--- 07_Collections/C03_Multi_Dimensional_Array/C03_Multi_Dimensional_Array/Program.cs
+++ 07_Collections/C03_Multi_Dimensional_Array/C03_Multi_Dimensional_Array/Program.cs
@@ -4,9 +4,39 @@ namespace C03_Multi_Dimensional_Array
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void PrintMatrix(int[,] matrix)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("[ ");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("   [ ");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + (j < matrix.GetLength(1) - 1 ? ", " : " "));
+                }
+                Console.WriteLine("]" + (i < matrix.GetLength(0) - 1 ? ", " : " "));
+            }
+
+            Console.WriteLine("]");
+        }
+
+        public static void Main(string[] args)
+        {
+            // declare 2D array
+            string[,] matrix;
+            // declare 3D array
+            int[,,] matrix3D;
+            // 2D array
+            int[,] array2D = new int[3, 3]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            Console.WriteLine($"Number of dimensions: {array2D.Rank}");
+            
+            PrintMatrix(array2D);
         }
     }
 }
---
+++ 07_Collections/C04_Nested_For_Loop/C04_Nested_For_Loop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace C04_Nested_For_Loop
+{
+    class Program
+    {
+        private static int[,] matrix =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+        
+        public static void Main(string[] args)
+        {
+            foreach (int num in matrix)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine("\n---------------------------------------------------------------");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
